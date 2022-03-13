ca1ed51e9eb47fbdda85ecef53247f6ee9184044
Giovane <giovanedaniell@gmail.com>
2022-03-13 15:03:47 -0300

    100% of the lint tests

--- playground-functions/challenges2.js
+++ playground-functions/challenges2.js
@@ -1,81 +1,80 @@
-// Desafio 10
-function techList(techArray, name) {
-  let objArray = [];
-
-  if (techArray.length === 0) {
-    return "Vazio!";
+// Desafio 11
+function checkLength(numberArray) {
+  if (numberArray.length !== 11) {
+    return true;
   }
-
-  for (let tech of techArray) {
-    let object = {
-      tech,
-      name,
-    };
-    objArray.push(object);
-  }
-
-  return objArray.sort((user1, user2) => {
-    return user1.tech < user2.tech ? -1 : 1;
-  });
 }
 
-// Desafio 11
-function generatePhoneNumber(numberArray) {
-  let counter = 0;
-
-  if (numberArray.length !== 11) {
-    return "Array com tamanho incorreto.";
-  }
-
+function isUnvalid(numberArray) {
   for (let number of numberArray) {
     if (number < 0 || number > 9) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return true;
     }
   }
+}
 
-  const occurrencesCounter = (array, number) =>
-    array.reduce(
-      (accumulator, indexValue) =>
-        indexValue === number ? accumulator + 1 : accumulator,
-      0
-    );
+function generateNumber(numberArray) {
+  numberArray = numberArray.join('');
+  return `(${numberArray.slice(0, 2)}) ${numberArray.slice(2, 7)}-${numberArray.slice(-4)}`;
+}
+
+const occurrencesCounter = function (array, number) {
+  return array.reduce((acc, value) => (value === number ? acc + 1 : acc), 0);
+};
+
+function generatePhoneNumber(numberArray) {
+  const length = checkLength(numberArray);
+  if (length) { return 'Array com tamanho incorreto.'; }
+
+  const unvalid = isUnvalid(numberArray);
+  if (unvalid) { return 'não é possível gerar um número de telefone com esses valores'; }
 
   for (let number of numberArray) {
     let occurrences = occurrencesCounter(numberArray, number);
     if (occurrences > 2) {
-      return "não é possível gerar um número de telefone com esses valores";
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
 
-  numberArray = numberArray.join("");
-
-  return `(${numberArray.slice(0, 2)}) ${numberArray.slice(
-    2,
-    7
-  )}-${numberArray.slice(-4)}`;
+  return generateNumber(numberArray);
 }
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-  if (lineA < lineB + lineC && lineA > Math.abs(lineB - lineC)) {
-    return true;
-  } else if (lineB < lineA + lineC && lineB > Math.abs(lineA - lineC)) {
-    return true;
-  } else if (lineC < lineB + lineA && lineC > Math.abs(lineB - lineA)) {
-    return true;
-  }
+function firstScenario(case1, case2) {
+  return case1 && case2;
+}
 
-  return false;
+function secondScenario(case1, case2) {
+  return case1 && case2;
+}
+
+function thirdScenario(case1, case2) {
+  return case1 && case2;
+}
+
+function triangleCheck(lineA, lineB, lineC) {
+  const case1 = (lineB + lineC > lineA);
+  const case2 = (lineA + lineC > lineB);
+  const case3 = (lineB + lineA > lineC);
+  const case4 = (lineA > Math.abs(lineB - lineC));
+  const case5 = (lineB > Math.abs(lineA - lineC));
+  const case6 = (lineC > Math.abs(lineB - lineA));
+
+  const condition1 = firstScenario(case1, case4);
+  const condition2 = secondScenario(case2, case5);
+  const condition3 = thirdScenario(case3, case6);
+
+  return condition1 || condition2 || condition3;
 }
 
 // Desafio 13
 function hydrate(string) {
-  let stringArray = string.split(" ");
+  let quantity;
+  let stringArray = string.split(' ');
   stringArray = stringArray.filter(Number);
-
   quantity = stringArray.reduce(
     (accumulator, arrayItem) => Number(accumulator) + Number(arrayItem),
-    0
+    0,
   );
 
   return quantity > 1
@@ -85,7 +84,6 @@ function hydrate(string) {
 
 module.exports = {
   generatePhoneNumber,
-  techList,
   hydrate,
   triangleCheck,
 };
